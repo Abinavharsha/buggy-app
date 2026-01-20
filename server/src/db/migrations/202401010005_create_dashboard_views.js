d2f08a2b85a5b1970671ee36e89b9696937fe50a
Abinavharsha <abhinav.harshavh@topgrep.com>
2026-01-20 12:13:07 +0530

    baseline

--- server/src/db/migrations/202401010005_create_dashboard_views.js
+++ server/src/db/migrations/202401010005_create_dashboard_views.js
@@ -3,6 +3,11 @@ export function up(knex) {
     table.increments("id").primary();
     table.integer("user_id").notNullable();
     table.timestamp("viewed_at").notNullable();
+    table.text("user_agent");
+    table.text("ip_address");
+    table.text("raw_headers");
+    table.text("session_dump");
+    table.text("debug_payload");
   });
 }
 
