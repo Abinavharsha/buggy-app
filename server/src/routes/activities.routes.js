e7ff12532d396bb9b35614642f3b9a7272f1c830
Abinavharsha <abhinav.harshavh@topgrep.com>
2026-01-23 15:43:20 +0530

    Module-3-Chapter-10

--- server/src/routes/activities.routes.js
+++ server/src/routes/activities.routes.js
@@ -3,8 +3,17 @@ import {
   getActivities,
   getActivity
 } from "../controllers/activities.controller.js";
-
+import {
+  getActivitiesUncompressed,
+  getActivitiesCompressedBuggy,
+  getActivitiesCompressedFixed,
+} from "../controllers/activities.compression.controller.js";
 const router = Router();
+
+// Chapter-10 compression endpoints
+router.get("/compression-off", getActivitiesUncompressed);
+router.get("/compression-buggy", getActivitiesCompressedBuggy);
+router.get("/compression-fixed", getActivitiesCompressedFixed);
 
 // GET /api/activities?page=1&limit=20&type=...
 router.get("/", getActivities);
